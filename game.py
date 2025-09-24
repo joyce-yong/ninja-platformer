0612ed77cae7a3e95ba940652c3f6dc565d776c2
Joyce <140150177+joyce-yong@users.noreply.github.com>
2025-09-24 10:51:56 +0800

    basic input

--- game.py
+++ game.py
@@ -10,12 +10,33 @@ class Game:
 
         self.clock = pygame.time.Clock() #fix fps
 
+        self.img = pygame.image.load('data/images/clouds/cloud1.png') #use png
+        self.img.set_colorkey((0, 0, 0)) #set colour key and make transparent
+
+        self.img_pos = [160, 260]
+        self.movement = [False, False]
+
     def run(self):
         while True:
+            self.screen.fill((14, 219, 248))
+
+            self.img_pos[1] += self.movement[1] - self.movement[0] * 5
+            self.screen.blit(self.img, self.img_pos) #top left is (0,0) screen is surface
+
             for event in pygame.event.get(): #handle input: touch, mouse click, resize, etc
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
 
             pygame.display.update()
             self.clock.tick(60)
--- main.py
+++
@@ -1,1 +0,0 @@
-print("hello ninja world")
